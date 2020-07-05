d660243687596895996c5b3f2fa385d4676ae2ce
Oliver Newland <newlao@amazon.com>
2020-07-05 09:04:27 -0700

    stupid money counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
+
+class MoneyViewer extends React.Component {
+  render() {
+    return <span id="moneyDisplay">${this.props.money}</span>
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {money: props.startMoney};
+    this.adjustMoneyCurry = this.adjustMoneyCurry.bind(this);
+  }
+
+  adjustMoneyCurry(amount) {
+    return () => this.setState({money: this.state.money + amount})
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <header>💯/3 Dopewars</header>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="space-children">
+            <header>Money</header>
+            <br/>
+            <Button onClick={this.adjustMoneyCurry(10)}>Increment</Button>
+            <Button onClick={this.adjustMoneyCurry(-10)}>Decrement</Button>
+            <MoneyViewer money={this.state.money}/>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game startMoney={100}/>
     </div>
   );
 }
